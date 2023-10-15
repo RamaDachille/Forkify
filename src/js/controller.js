7ac433bff902aeb94ab06defa031c1f73020d266
Rama DAchille <dachille108@gmail.com>
2023-10-15 20:58:22 +0200

    rendering error messages in view

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,7 +19,7 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error('Controller:', err);
+    recipeView.renderError()
   }
 };
 
